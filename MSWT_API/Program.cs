213f77f158a1a32727da518977b730446ac9b37f
Khang đi học <127173986+KhangHocHanh@users.noreply.github.com>
2025-06-20 13:20:26 +0700

    Commit

--- MSWT_API/Program.cs
+++ MSWT_API/Program.cs
@@ -132,12 +132,19 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction() || app.Environment.IsStaging())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 
 app.UseHttpsRedirection();
