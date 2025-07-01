2c5f242fa8de57bd423dad5ba2fe89150ceb37ca
Khang đi học <127173986+KhangHocHanh@users.noreply.github.com>
2025-07-01 15:41:38 +0700

    Get all, get by id of alert

---
+++ MSWT_Services/Services/AlertService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MSWT_BussinessObject.Model;
+using MSWT_Repositories.IRepository;
+using MSWT_Services.IServices;
+
+namespace MSWT_Services.Services
+{
+    public class AlertService : IAlertService
+    {
+        private readonly IAlertRepository _alertRepository;
+        public AlertService(IAlertRepository alertRepository)
+        {
+            _alertRepository = alertRepository;
+        }
+        public async Task<IEnumerable<Alert>> GetAllAlerts()
+        {
+            return await _alertRepository.GetAllAsync();
+        }
+        public async Task<Alert> GetAlertById(string id)
+        {
+            return await _alertRepository.GetByIdAsync(id);
+        }
+        public async Task DeleteAlert(string id)
+        {
+            await _alertRepository.DeleteAsync(id);
+        }
+    }
+}
