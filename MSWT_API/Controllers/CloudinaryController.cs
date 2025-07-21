15a1e92053f975fd570b255d3078f4089807928d
predrir <predrirvn@gmail.com>
2025-07-21 13:02:18 +0700

    Added cloudinary and updated API for CreateScheduleDetail

---
+++ MSWT_API/Controllers/CloudinaryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSWT_BussinessObject.RequestDTO;
+using MSWT_Services.IServices;
+using MSWT_Services.Services;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CloudinaryController : ControllerBase
+    {
+        private readonly ICloudinaryService _cloudinaryService;
+
+        public CloudinaryController(ICloudinaryService cloudinaryService)
+        {
+            _cloudinaryService = cloudinaryService;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            try
+            {
+                var result = await _cloudinaryService.UploadFile(file);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
