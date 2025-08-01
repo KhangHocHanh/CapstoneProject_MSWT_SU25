c8af2171026fad74440c31a4769d86117b517ec7
Khang đi học <127173986+KhangHocHanh@users.noreply.github.com>
2025-08-01 20:38:23 +0700

    Update TrashBin to get sensor name

--- MSWT_Repositories/Repository/TrashBinRepository.cs
+++ MSWT_Repositories/Repository/TrashBinRepository.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MSWT_BussinessObject.Model;
 using MSWT_Repositories.IRepository;
+using static MSWT_BussinessObject.ResponseDTO.ResponseDTO;
 
 namespace MSWT_Repositories.Repository
 {
@@ -48,6 +49,30 @@ namespace MSWT_Repositories.Repository
         {
             _context.TrashBins.Update(TrashBin);
             await _context.SaveChangesAsync();
+        }
+        public async Task<List<TrashBinWithSensorDTO>> GetTrashBinsWithSensorsAsync()
+        {
+            var result = await _context.SensorBins
+       .Include(sb => sb.Bin)
+       .Include(sb => sb.Sensor)
+       .GroupBy(sb => sb.Bin.TrashBinId)
+       .Select(g => new TrashBinWithSensorDTO
+       {
+           TrashBinId = g.Key,
+           Status = g.First().Bin.Status,
+           AreaId = g.First().Bin.AreaId,
+           Location = g.First().Bin.Location,
+           Image = g.First().Bin.Image,
+           RestroomId = g.First().Bin.RestroomId,
+           Sensors = g.Select(sb => new SensorInfoDTO
+           {
+               SensorId = sb.Sensor.SensorId,
+               SensorName = sb.Sensor.SensorName
+           }).ToList()
+       })
+       .ToListAsync();
+
+            return result;
         }
     }
 }
--- MSWT_Services/Services/TrashBinService.cs
+++ MSWT_Services/Services/TrashBinService.cs
@@ -47,5 +47,9 @@ namespace MSWT_Services.Services
         {
             await _TrashBinRepository.UpdateAsync(TrashBin);
         }
+        public async Task<List<TrashBinWithSensorDTO>> GetTrashBinsWithSensorsAsync()
+        {
+            return await _TrashBinRepository.GetTrashBinsWithSensorsAsync();
+        }
     }
 }
