74a899746f0a1732d53ec3295be6f0d9dead5518
predrir <predrirvn@gmail.com>
2025-08-29 12:29:28 +0700

    Adjusted api and added getScheduleDetailsbyDateandUser

--- MSWT_Repositories/IRepository/IScheduleDetailsRepository.cs
+++ MSWT_Repositories/IRepository/IScheduleDetailsRepository.cs
@@ -16,6 +16,8 @@ namespace MSWT_Repositories.IRepository
         Task UpdateAsync(ScheduleDetail scheduleDetail);
         Task<IEnumerable<ScheduleDetail>> GetAllAsync();
         Task<IEnumerable<ScheduleDetail>> SearchByUserIdAsync(string userId);
+        Task<IEnumerable<ScheduleDetail>> GetByUserIdAndDateAsync(string userId, DateTime date);
+        Task<(IEnumerable<ScheduleDetail> Items, int TotalCount)> GetByDatePaginatedAsync(DateTime date, int pageNumber, int pageSize);
         //Task<ScheduleDetail?> GetByUserAndDateAsync(string userId, DateOnly targetDate);
         //Task<double?> GetAverageRatingForMonthAsync(string workerId, int year, int month);
         //Task<(int workedDays, int totalDays, double percentage)> GetWorkStatsInMonthAsync(string workerId, int month, int year);
